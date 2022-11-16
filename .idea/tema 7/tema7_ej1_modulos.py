af478110cebde1e210abe38b15cca529ecd7e717
Miguel <miguecuevass@gmail.com>
2022-11-16 13:05:52 +0100

    Fourth commit tema 7 ej 1 y cambios tema 6

---
+++ .idea/tema 7/tema7_ej1_modulos.py
@@ -0,0 +1,13 @@
+import calculadora.calculadora as c
+
+suma1 = c.suma(2 ,2)
+print(suma1)
+
+resta1 = c.restar(2 ,2)
+print(resta1)
+
+mult1 = c.multiplicar(2 ,3)
+print(mult1)
+
+div1 = c.dividir(2 ,2)
+print(div1)
